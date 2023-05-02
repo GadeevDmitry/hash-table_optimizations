a0e48aadec2a5bf2b182226cf8e617a6a6eda6af
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-05-03 01:44:59 +0300

    potential v3 (cache key)

--- src/search.cpp
+++ src/search.cpp
@@ -31,7 +31,7 @@ const char  *HASH_TABLE_TEXT = "data/dictionary.txt";
 int        (*HASH_TABLE_CMP ) (hash_key fst, hash_key sec) = strcmp;
 hash_val   (*HASH_TABLE_CALC) (hash_key elem)              = hash_crc32;
 
-const int RUN_SEARCH_NUM      = 3000;
+const int RUN_SEARCH_NUM      = 5000;
 const int MAX_DICTIONARY_SIZE = 60000;
 
 //--------------------------------------------------------------------------------------------------------------------------------
@@ -42,7 +42,9 @@ void run_search();
 
 static double               run_search       (const hash_table *const store, hash_key *lexis_array);
 static __always_inline void hash_table_search(const hash_table *const store, hash_key *lexis_array);
-static hash_table          *hash_table_init  (                               hash_key *lexis_array);
+
+static hash_table          *hash_table_init   (                               hash_key *lexis_array);
+static char                *hash_table_rebuild(hash_table *const store, const size_t total_key_size);
 
 static hash_key *lexis_array_init(buffer *const dictionary);
 
@@ -62,12 +64,15 @@ void run_search()
     buffer *dictionary    = buffer_new (HASH_TABLE_TEXT);
     hash_key *lexis_array = lexis_array_init(dictionary);
     hash_table *store     = hash_table_init(lexis_array);
+    char *new_key_store   = hash_table_rebuild(store, dictionary->buff_size);
 
     fprintf(stderr, "search time: %lf ms\n", run_search(store, lexis_array));
 
     buffer_free(dictionary);
-    log_free  (lexis_array);
     hash_table_free (store);
+
+    log_free(new_key_store);
+    log_free(lexis_array);
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------
@@ -125,6 +130,37 @@ static hash_table *hash_table_init(hash_key *lexis_array)
     }
 
     return store;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+static char *hash_table_rebuild(hash_table *const store, const size_t total_key_size)
+{
+    log_assert(store != nullptr);
+
+    char *const new_key_store = (char *) log_calloc(total_key_size, sizeof(char));
+    log_assert(new_key_store != nullptr);
+
+    char *next_key_addr = new_key_store;
+    for (size_t chain = 0; chain < HASH_TABLE_SIZE; ++chain)
+    {
+        list_node *dup_fict = store->data[chain].fictional;
+        list_node *dup_cur  = dup_fict + dup_fict->next;
+
+        while (dup_cur != dup_fict)
+        {
+            size_t key_len = strlen((const char *) dup_cur->data) + 1; //null-character
+
+            memcpy(next_key_addr, dup_cur->data, key_len);
+
+            dup_cur->data = next_key_addr;
+            dup_cur = dup_fict + dup_cur->next;
+
+            next_key_addr += key_len;
+        }
+    }
+
+    return new_key_store;
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------
