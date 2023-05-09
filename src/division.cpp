33e13d0b7403ec3ea6e116af9191e517b0249f83
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-05-09 15:45:24 +0300

    data refactoting

--- src/division.cpp
+++ src/division.cpp
@@ -17,7 +17,7 @@
 // SETTINGS
 //--------------------------------------------------------------------------------------------------------------------------------
 
-const size_t HASH_TABLE_SIZE = 1907;
+const size_t HASH_TABLE_SIZE = 5701;
 const char  *HASH_TABLE_TEXT = "data/dictionary.txt";
 const char  *HASH_TABLE_CSV  = "data/division.csv";
 
